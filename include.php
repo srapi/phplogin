06c62b76f1f6de0f91a2d66aa4b02c80946074a9
srapi <101911727+srapi@users.noreply.github.com>
2022-03-19 07:37:34 +0530

    php files uploaded
    php files uploaded

---
+++ include.php
@@ -0,0 +1,16 @@
+<html>
+<body>
+<?php include 'menu.php';?>
+<?php require 'vars.php';?>
+<h1>Welcome to my home page!</h1>
+<p>Some text.</p>
+<p>Some more text.</p>
+
+echo "I have a $color $car.";
+<?php include 'footer.php';?>
+<?php include 'vars.php';
+echo "I have a $color $car.";
+?>
+
+</body>
+</html>
---
+++ welcome_get.php
@@ -0,0 +1,8 @@
+<html>
+<body>
+sardar gotting
+Welcome <?php echo $_GET["name"]; ?><br>
+Your email address is: <?php echo $_GET["email"]; ?>
+
+</body>
+</html>
